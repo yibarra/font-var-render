77579fcb696020c4368060adc1bdd77c91579560
Julian Ibarra <yeissonibarra@gmail.com>
2020-07-28 21:57:56 +0100

    Updates

---
+++ precache-manifest.1f1a32cd390d102c05d5144ce2da9628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd55974c26b7a69b8a73b1eee8101db",
+    "url": "/font-var-render/index.html"
+  },
+  {
+    "revision": "e6aaa80cc139a7132c95",
+    "url": "/font-var-render/static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "b6239c52265a42376b77",
+    "url": "/font-var-render/static/css/main.234650e8.chunk.css"
+  },
+  {
+    "revision": "e6aaa80cc139a7132c95",
+    "url": "/font-var-render/static/js/2.db4ce398.chunk.js"
+  },
+  {
+    "revision": "1502c80527df6509e2b003d6a76dbea1",
+    "url": "/font-var-render/static/js/2.db4ce398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6239c52265a42376b77",
+    "url": "/font-var-render/static/js/main.8495d4d8.chunk.js"
+  },
+  {
+    "revision": "ce77a74f7aa905924192",
+    "url": "/font-var-render/static/js/runtime-main.456a6f47.js"
+  }
+]);
